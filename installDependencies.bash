263201a5a2d4e57cf49c536346164031fc99ccbf
Desperationis <smarttdiego@gmail.com>
2021-05-22 20:24:03 -0700

    Use SDL in linux
    For linux, use the SDL packages that are should've been installed as
    told in README. This also has support for Windows were SDL is defined in
    project.

--- installDependencies.bash
+++ installDependencies.bash
@@ -1,4 +1,5 @@
-# Install SDL2, SDL2_image, SDL2_TTF, json_develop, and hopscotch_map
+# Install json_develop and hopscotch_map
+# Unix only
 
 
 # Make and clean dirs
@@ -12,79 +13,8 @@ then
 	rm -rf Dependencies
 fi
 
-if [[ -d executable ]]
-then
-	rm -rf executable
-fi
-
 mkdir tmp
 mkdir Dependencies
-mkdir executable
-
-cd Dependencies
-mkdir SDL_FULL
-cd SDL_FULL
-mkdir x86
-mkdir include
-cd ../../
-
-# Install SDL2
-cd tmp
-if ! wget https://www.libsdl.org/release/SDL2-devel-2.0.14-VC.zip
-then
-	echo "Could not download SDL2."
-	exit 1
-fi
-
-unzip SDL2-devel-2.0.14-VC.zip
-
-rm SDL2-devel-2.0.14-VC.zip
-
-cd ../
-
-mv tmp/SDL2-2.0.14/include/*.h Dependencies/SDL_FULL/include
-mv tmp/SDL2-2.0.14/lib/x86/*.lib Dependencies/SDL_FULL/x86
-mv tmp/SDL2-2.0.14/lib/x86/*.txt executable
-mv tmp/SDL2-2.0.14/lib/x86/*.dll executable
-
-# Install SDL2_image
-cd tmp
-if ! wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip
-then 
-	echo "Could not download SDL2_image."
-	exit 1
-fi
-
-unzip SDL2_image-devel-2.0.5-VC.zip
-
-rm SDL2_image-devel-2.0.5-VC.zip
-
-cd ../
-
-
-mv tmp/SDL2_image-2.0.5/include/*.h Dependencies/SDL_FULL/include/
-mv tmp/SDL2_image-2.0.5/lib/x86/*.lib Dependencies/SDL_FULL/x86
-mv tmp/SDL2_image-2.0.5/lib/x86/*.txt executable
-mv tmp/SDL2_image-2.0.5/lib/x86/*.dll executable
-
-# Install SDL2_TTF
-cd tmp
-if ! wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip
-then 
-	echo "Could not download SDL2_ttf"
-	exit 1
-fi
-
-unzip SDL2_ttf-devel-2.0.15-VC.zip
-
-rm SDL2_ttf-devel-2.0.15-VC.zip
-
-cd ../
-
-mv tmp/SDL2_ttf-2.0.15/include/*.h Dependencies/SDL_FULL/include
-mv tmp/SDL2_ttf-2.0.15/lib/x86/*.lib Dependencies/SDL_FULL/x86
-mv tmp/SDL2_ttf-2.0.15/lib/x86/*.txt executable
-mv tmp/SDL2_ttf-2.0.15/lib/x86/*.dll executable
 
 # Install nlohmann/json
 cd tmp
